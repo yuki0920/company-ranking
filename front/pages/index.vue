898385d9b5f9f4450cd8ea0288d787362f3af2df
yuki0920 <ywsep20@gmail.com>
2021-07-18 12:38:33 +0900

    Add local api connect settings

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <Tutorial />
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
+<script>
+export default {
+  data () {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.$get('/api/v1/hello')
+        .then(res => this.msgs.push(res))
+    }
+  }
+}
 </script>
