7f8e52d34f16974e5a9f6209178bb842325d7e6b
yuki0920 <ywsep20@gmail.com>
2021-07-20 08:26:52 +0900

    Add Company

---
+++ front/pages/companies/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-breadcrumb class="bg-white">
+      <b-breadcrumb-item to="/">
+        トップ
+      </b-breadcrumb-item>
+      <b-breadcrumb-item to="/companies">
+        企業
+      </b-breadcrumb-item>
+      <b-breadcrumb-item active>
+        {{ company.name }}
+      </b-breadcrumb-item>
+    </b-breadcrumb>
+    <div class="container">
+      <company :company="company" />
+    </div>
+  </div>
+</template>
+
+<script>
+import company from '~/lib/company.js'
+export default {
+  data () {
+    return {
+      company
+    }
+  }
+}
+</script>
