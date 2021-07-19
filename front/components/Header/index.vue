db890378079e73873a36b98b5f5aca65a980662d
yuki0920 <ywsep20@gmail.com>
2021-07-19 22:26:28 +0900

    Add Header

---
+++ front/components/Header/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-navbar toggleable="sm" type="dark" variant="gray-dark">
+      <div class="container">
+        <b-navbar-brand to="/">
+          上場企業ランキング
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse" />
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">
+              企業
+            </b-nav-item>
+            <b-nav-item to="/">
+              業界
+            </b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search" />
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">
+                Search
+              </b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </div>
+    </b-navbar>
+  </div>
+</template>
