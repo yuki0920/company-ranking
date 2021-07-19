24313404972d259b3f173401aea8a1736b958be1
yuki0920 <ywsep20@gmail.com>
2021-07-19 22:57:10 +0900

    Remove bootstrap.scss

--- front/components/Header/index.vue
+++ front/components/Header/index.vue
@@ -18,8 +18,8 @@
           <b-navbar-nav class="ml-auto">
             <b-nav-form>
               <b-form-input size="sm" class="mr-sm-2" placeholder="Search" />
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">
-                Search
+              <b-button size="sm" variant="light" class="my-2 my-sm-0" type="submit">
+                検索する
               </b-button>
             </b-nav-form>
           </b-navbar-nav>
