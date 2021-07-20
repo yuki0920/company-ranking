0d3a3e0c54b0622b3fd2dffb667182be09a9e9d2
yuki0920 <ywsep20@gmail.com>
2021-07-21 08:39:32 +0900

    Add tables

--- api/app/models/industry.rb
+++ api/app/models/industry.rb
@@ -1,37 +1,45 @@
-class Industry < ActiveHash::Base
-  self.data = [
-    { id: 1, name: '水産・農林業', code: 50 },
-    { id: 2, name: '鉱業', code: 1050 },
-    { id: 3, name: '建設業', code: 2050 },
-    { id: 4, name: '食料品', code: 3050 },
-    { id: 5, name: '繊維製品', code: 3100 },
-    { id: 6, name: 'パルプ・紙', code: 3150 },
-    { id: 7, name: '化学', code: 3200 },
-    { id: 8, name: '医薬品', code: 3250 },
-    { id: 9, name: '石油・石炭製品', code: 3300 },
-    { id: 10, name: 'ゴム製品', code: 3350 },
-    { id: 11, name: 'ガラス・土石製品', code: 3400 },
-    { id: 12, name: '鉄鋼', code: 3450 },
-    { id: 13, name: '非鉄金属', code: 3500 },
-    { id: 14, name: '金属製品', code: 3550 },
-    { id: 15, name: '機械', code: 3600 },
-    { id: 16, name: '電気機器', code: 3650 },
-    { id: 17, name: '輸送用機器', code: 3700 },
-    { id: 18, name: '精密機器', code: 3750 },
-    { id: 19, name: 'その他製品', code: 3800 },
-    { id: 20, name: '電気・ガス業', code: 4050 },
-    { id: 21, name: '陸運業', code: 5050 },
-    { id: 22, name: '海運業', code: 5100 },
-    { id: 23, name: '空運業', code: 5150 },
-    { id: 24, name: '倉庫・運輸関連業', code: 5200 },
-    { id: 25, name: '情報・通信業', code: 5250 },
-    { id: 26, name: '卸売業', code: 6050 },
-    { id: 27, name: '小売業', code: 6100 },
-    { id: 28, name: '銀行業', code: 7050 },
-    { id: 29, name: '証券、商品先物取引業', code: 7100 },
-    { id: 30, name: '保険業', code: 7150 },
-    { id: 31, name: 'その他金融業', code: 7200 },
-    { id: 32, name: '不動産業', code: 8050 },
-    { id: 33, name: 'サービス業', code: 9050 }
-  ]
+class Industry  < ApplicationRecord
+  INITIAL_DATA = [
+    { name: '水産・農林業', code: 50 },
+    { name: '鉱業', code: 1050 },
+    { name: '建設業', code: 2050 },
+    { name: '食料品', code: 3050 },
+    { name: '繊維製品', code: 3100 },
+    { name: 'パルプ・紙', code: 3150 },
+    { name: '化学', code: 3200 },
+    { name: '医薬品', code: 3250 },
+    { name: '石油・石炭製品', code: 3300 },
+    { name: 'ゴム製品', code: 3350 },
+    { name: 'ガラス・土石製品', code: 3400 },
+    { name: '鉄鋼', code: 3450 },
+    { name: '非鉄金属', code: 3500 },
+    { name: '金属製品', code: 3550 },
+    { name: '機械', code: 3600 },
+    { name: '電気機器', code: 3650 },
+    { name: '輸送用機器', code: 3700 },
+    { name: '精密機器', code: 3750 },
+    { name: 'その他製品', code: 3800 },
+    { name: '電気・ガス業', code: 4050 },
+    { name: '陸運業', code: 5050 },
+    { name: '海運業', code: 5100 },
+    { name: '空運業', code: 5150 },
+    { name: '倉庫・運輸関連業', code: 5200 },
+    { name: '情報・通信業', code: 5250 },
+    { name: '卸売業', code: 6050 },
+    { name: '小売業', code: 6100 },
+    { name: '銀行業', code: 7050 },
+    { name: '証券、商品先物取引業', code: 7100 },
+    { name: '保険業', code: 7150 },
+    { name: 'その他金融業', code: 7200 },
+    { name: '不動産業', code: 8050 },
+    { name: 'サービス業', code: 9050 }
+  ].freeze
+
+  class << self
+    def create_seed
+      INITIAL_DATA.each do |data|
+        create!(data)
+      end
+    end
+  end
 end
--- api/db/migrate/20210720221801_add_securities.rb
+++ api/db/migrate/20210720221801_add_securities.rb
@@ -1,12 +1,17 @@
 class AddSecurities < ActiveRecord::Migration[6.1]
   def change
     create_table :securities do |t|
-      t.integer :code, null: false, unique: true
-      t.string :name, null: false, unique: true
-      t.integer :market_id, null: false
+      t.integer :code, null: false
+      t.string :name, null: false
+      t.belongs_to :market, null: false, index: true
       t.integer :industry_code, null: false
 
       t.timestamps
     end
+
+    add_foreign_key :securities, :industries, column: :industry_code, primary_key: :code
+    add_index :securities, :code, unique: true
+    add_index :securities, :name, unique: true
+    add_index :securities, :industry_code
   end
 end
