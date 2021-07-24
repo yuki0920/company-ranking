4135c2e447617a724892cf9f5795f4af809656ff
yuki0920 <ywsep20@gmail.com>
2021-07-24 14:56:26 +0900

    Add favicon

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/file-bar-graph.ico' }
     ]
   },
 
