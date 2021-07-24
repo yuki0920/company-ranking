c44ddbb7a61aa940b69cc4f45f73dfec9d7bf410
yuki0920 <ywsep20@gmail.com>
2021-07-24 22:23:50 +0900

    Add border to list for mobile

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -4,7 +4,7 @@
     <section class="industries">
       <h5>業種から探す</h5>
       <ul class="row list-unstyled">
-        <li v-for="industry in industries" :key="`industry-${industry.id}`" class="col-sm-6 col-md-3">
+        <li v-for="industry in industries" :key="`industry-${industry.id}`" class="col-6 col-md-3" :class="{ 'border': isMobile }">
           {{ industry.name }}
         </li>
       </ul>
@@ -12,7 +12,7 @@
     <section class="markets">
       <h5>市場から探す</h5>
       <ul class="row list-unstyled">
-        <li v-for="market in markets" :key="`market-${market.id}`" class="col-sm-6 col-md-3">
+        <li v-for="market in markets" :key="`market-${market.id}`" class="col-6 col-md-3" :class="{ 'border': isMobile }">
           {{ market.name }}
         </li>
       </ul>
@@ -24,10 +24,13 @@
 import { defineComponent } from '@nuxtjs/composition-api'
 import industries from '~/lib/industries'
 import markets from '~/lib/markets'
+import { useUtility } from '~/lib/utility'
 
 export default defineComponent({
   setup () {
+    const { isMobile } = useUtility()
     return {
+      isMobile,
       industries,
       markets
     }
