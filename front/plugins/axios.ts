1f0103d9db446da91228a91e6bdbcdf583d77f66
yuki0920 <ywsep20@gmail.com>
2021-07-27 08:01:08 +0900

    Remove log from axios in production

--- front/plugins/axios.ts
+++ front/plugins/axios.ts
@@ -1,11 +1,13 @@
-export default ({ $axios }: {$axios: any}) => {
-  $axios.onRequest((config: any) => {
-    console.log(config)
-  })
-  $axios.onResponse((config: any) => {
-    console.log(config)
-  })
-  $axios.onError((e: any) => {
-    console.log(e.response)
-  })
+export default ({ $axios }: { $axios: any }) => {
+  if (process.env.NODE_ENV !== 'development') {
+    $axios.onRequest((config: any) => {
+      console.log(config)
+    })
+    $axios.onResponse((config: any) => {
+      console.log(config)
+    })
+    $axios.onError((e: any) => {
+      console.log(e.response)
+    })
+  }
 }
