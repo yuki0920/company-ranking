371013cb8d898b827f8c9f15ec2c6491521587b4
yuki0920 <ywsep20@gmail.com>
2021-08-04 02:49:02 +0900

    Add financials to `GET /api/v1/companies/:id`

--- api/app/helpers/application_helper.rb
+++ api/app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def divved_number(number, rank)
     number ? number / rank : nil
   end
+
+  def format_ratio(number)
+    return nil if number.nil? || number > 1
+
+    (number * 100).round(1)
+  end
 end
