7e3d8bbb921b51775b224e780a74f49a49701e83
yuki0920 <ywsep20@gmail.com>
2021-08-07 20:51:55 +0900

    Add keyword

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -18,6 +18,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { hid: 'keyword', name: 'keyword', content: '業績,売上,利益,年収,上場企業,ランキング' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
