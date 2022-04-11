93a9c97708ea02994accadde8a2679710afb6290
yuki0920 <ywsep20@gmail.com>
2022-04-11 18:12:19 +0900

    Remove old securities when duplicate company name exists

--- api/app/models/security.rb
+++ api/app/models/security.rb
@@ -3,7 +3,7 @@
 class Security < ApplicationRecord
   belongs_to :market
   belongs_to :industry, foreign_key: :industry_code, primary_key: :code
-  has_many :documents, foreign_key: :security_code, primary_key: :code
+  has_many :documents, foreign_key: :security_code, primary_key: :code, dependent: :destroy
 
   validates :code, uniqueness: true, presence: true
   validates :name, uniqueness: true, presence: true
