96950a2e086aaa8a7ca5ebed6a3339f4b4f74412
yuki0920 <ywsep20@gmail.com>
2023-06-02 14:04:08 +0900

    Update logger

--- go/middleware/logger.go
+++ go/middleware/logger.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -17,7 +16,6 @@ func Logger(l *zap.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
-			fmt.Println("called!!")
 			t1 := time.Now()
 			defer func() {
 				l.Info("Served",
