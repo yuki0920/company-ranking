a2fc1aa6ab1b62ef8775b84cae766e9b2bd2b907
yuki0920 <ywsep20@gmail.com>
2023-06-03 18:22:10 +0900

    Remove unused cors setting

--- ruby/config/initializers/cors.rb
+++ ruby/config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV['FRONT_DOMAIN'], 'company-ranking.net', 'company-ranking.netlify.app'
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins ENV['FRONT_DOMAIN'], 'company-ranking.net', 'company-ranking.netlify.app'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
