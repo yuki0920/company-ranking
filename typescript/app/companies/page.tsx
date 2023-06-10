73bd8a2ca9be8b1f3cae5d9575c44794284fd746
yuki0920 <ywsep20@gmail.com>
2023-06-10 17:36:35 +0900

    Add wip search action

--- typescript/app/companies/page.tsx
+++ typescript/app/companies/page.tsx
@@ -3,25 +3,41 @@
 import { DefaultApi, Configuration } from "@/client"
 import { BASE_PATH } from "@/constant"
 import Link from 'next/link'
-import { useSearchParams } from 'next/navigation'
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
 import { numberWithDelimiter, divide_1_000, divide_1_000_000 } from "@/lib/utility"
+import { useState, ChangeEventHandler, MouseEventHandler } from "react"
 
 export default async function Page() {
+  const [query, setQuery] = useState("")
+  const router = useRouter()
+  const pathname = usePathname()
   const searchParams = useSearchParams()
+
   const currentPage = searchParams.get('page') ? Number(searchParams.get('page')) : 1
   const { companies, meta } = await getCompanies(currentPage)
   const { from, page, prev, next } = meta
+  const handleSearchChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    setQuery(target.value)
+  }
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault()
+    const params = new URLSearchParams(searchParams.toString())
+    const path = pathname + '?' + params.set('q', query)
+    router.push(path)
+  }
 
   return (
     <div className="overflow-x-auto">
+      {/* form */}
       <div className="form-control">
         <div className="input-group">
-          <input type="text" placeholder="社名 または 証券コード" className="input input-bordered w-full" />
-          <button className="btn btn-square">
+          <input type="text" placeholder="社名 または 証券コード" className="input input-bordered w-full" value={query} onChange={handleSearchChange} />
+          <button className="btn btn-square" onClick={handleClick}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
           </button>
         </div>
       </div>
+      {/* table */}
       <table className="table table-sm">
         <thead>
           <tr className="text-left">
@@ -48,6 +64,7 @@ export default async function Page() {
           })}
         </tbody>
       </table>
+      {/* pagination */}
       <div className="flex items-center justify-center">
         <div className="join">
           <button className={`join-item btn btn-sm ${prev ?? "btn-disabled"}`}>
