3586cc2d6d55e42447db65b6cafbbb62781e61c8
yuki0920 <ywsep20@gmail.com>
2023-06-10 13:54:10 +0900

    Add search button

--- typescript/app/companies/page.tsx
+++ typescript/app/companies/page.tsx
@@ -14,6 +14,14 @@ export default async function Page() {
 
   return (
     <div className="overflow-x-auto">
+      <div className="form-control">
+        <div className="input-group">
+          <input type="text" placeholder="社名 または 証券コード" className="input input-bordered w-full" />
+          <button className="btn btn-square">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+          </button>
+        </div>
+      </div>
       <table className="table table-sm">
         <thead>
           <tr className="text-left">
