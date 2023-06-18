081db48bdcdc509b6c7f5e3a0b0b5754695cdf23
yuki0920 <ywsep20@gmail.com>
2023-06-18 09:54:31 +0900

    Add SortButtons component

--- typescript/pages/markets/[id]/index.tsx
+++ typescript/pages/markets/[id]/index.tsx
@@ -4,6 +4,7 @@ import { DefaultApi, Configuration, FetchCompaniesSortTypeEnum, EachCompany, Met
 import { NEXT_PUBLIC_API_URL } from "@/constant"
 import CompanyTable from "@/components/CompanyTable"
 import SearchBox from "@/components/SearchBox"
+import SortButtons from "@/components/SortButtons"
 import Pagination from "@/components/Pagination"
 
 export default function Markets({ companies, meta }: { companies: EachCompany[], meta: Meta }) {
@@ -11,6 +12,12 @@ export default function Markets({ companies, meta }: { companies: EachCompany[],
   const router = useRouter()
   const { page, from, prev, next } = meta
   const { id } = router.query
+  let currentSortType: string
+  if (router.query.sortType === undefined) {
+    currentSortType = "net_sales"
+  } else {
+    currentSortType = router.query.sortType as string
+  }
 
   const handleQuery: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
     setQuery(target.value)
@@ -21,7 +28,19 @@ export default function Markets({ companies, meta }: { companies: EachCompany[],
     router.push({
       pathname: `/markets/${id}`,
       query: {
-        q: query
+        sortType: currentSortType,
+        q: query,
+      }
+    })
+  }
+
+  const handleSortType: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const changedSortType = e.target.value as FetchCompaniesSortTypeEnum
+    router.push({
+      pathname: `/markets/${id}`,
+      query: {
+        sortType: changedSortType,
+        q: query,
       }
     })
   }
@@ -31,6 +50,10 @@ export default function Markets({ companies, meta }: { companies: EachCompany[],
       {/* search */}
       <SearchBox query={query} handleQuery={handleQuery} handleSearch={handleSearch} />
       {/* search */}
+
+      {/* sort */}
+      <SortButtons currentSortType={currentSortType} handleSortType={handleSortType} />
+      {/* sort */}
 
       {/* companies */}
       <CompanyTable companies={companies} from={from} />
