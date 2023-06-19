d21a45339438f62fb37c98912c80a7bc59cbeb50
yuki0920 <ywsep20@gmail.com>
2023-06-19 21:00:25 +0900

    Display companies

---
+++ typescript/app/tests/page.tsx
@@ -0,0 +1,33 @@
+import { DefaultApi, Configuration, FetchCompaniesSortTypeEnum } from "@/client"
+import { NEXT_PUBLIC_API_URL } from "@/constant"
+import CompanyTable from "@/components/CompanyTable"
+
+export default async function Page(
+  { searchParams: {sortType = "net_sales", page = 1, q = "" }}:
+  { searchParams: { page: number | undefined, sortType: FetchCompaniesSortTypeEnum, q: string | undefined } })
+{
+  const { companies, meta } = await getCompanies({ page, sortType, q })
+  const { from } = meta
+
+  return (
+    <>
+      {/* companies */}
+        <CompanyTable companies={companies} from={from} />
+      {/* companies */}
+    </>
+  )
+}
+const getCompanies = async (
+  {page, sortType, q}:
+  { page: number, sortType: FetchCompaniesSortTypeEnum, q: string }
+) => {
+  const config = new Configuration({ basePath: NEXT_PUBLIC_API_URL })
+  const DefaultAPI = new DefaultApi(config)
+  const { companies, meta } = await DefaultAPI.fetchCompanies({
+    page: page,
+    sortType: sortType,
+    q: q,
+  })
+
+  return { companies, meta }
+}
