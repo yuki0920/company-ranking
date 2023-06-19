2a211ed891f6c684bf1ef34a76334ce56da5c054
yuki0920 <ywsep20@gmail.com>
2023-06-19 21:25:50 +0900

    Add Pagination

--- typescript/app/tests/test-companies.tsx
+++ typescript/app/tests/test-companies.tsx
@@ -5,12 +5,13 @@ import {  EachCompany, Meta } from "@/client"
 import SearchBox from "@/components/SearchBox"
 import CompanyTable from "@/components/CompanyTable"
 import { useRouter, useSearchParams } from "next/navigation"
+import Pagination from "@/components/Pagination"
 
 export default function TestCompanies({ companies, meta }: { companies: EachCompany[], meta: Meta }) {
   const [query, setQuery] = useState("")
   const router = useRouter()
   const searchParams = useSearchParams()
-  const { from } = meta
+  const { page, from, prev, next } = meta
 
   const handleQuery: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
     setQuery(target.value)
@@ -45,6 +46,16 @@ export default function TestCompanies({ companies, meta }: { companies: EachComp
       {/* companies */}
       <CompanyTable companies={companies} from={from} />
       {/* companies */}
+
+      {/* pagination */}
+      <Pagination
+        prevRef={{ pathname: '/companies', query: { page: page - 1 }}}
+        nextRef={{ pathname: '/companies', query: { page: page + 1 }}}
+        page={page}
+        prev={prev}
+        next={next}
+      />
+      {/* pagination */}
     </>
   )
 }
