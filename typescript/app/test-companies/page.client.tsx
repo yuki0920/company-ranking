eeb23ec45a8e388ddf3232fd5449dbfc936ccc07
yuki0920 <ywsep20@gmail.com>
2023-06-22 22:04:18 +0900

    Add pagination to app

--- typescript/app/test-companies/page.client.tsx
+++ typescript/app/test-companies/page.client.tsx
@@ -3,33 +3,66 @@
 import { useEffect, useState } from "react"
 import CompanyTable from "./CompanyTable"
 import { EachCompanyJSON, MetaJSON } from "@/types"
+import Pagination from "./Pagination"
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 export function ClientCompanies() {
   const [companies, setCompanies] = useState<EachCompanyJSON[]>([])
   const [from, setFrom] = useState<number>(1)
+  const [prev, setPrev] = useState<number | null>(null)
+  const [next, setNext] = useState<number | null>(null)
+  const searchParams = useSearchParams()
+  const currentPage = searchParams !== null && searchParams.get('page') ? Number(searchParams.get('page')) : 1
+  const [page, setPage] = useState(currentPage)
+
+  const router = useRouter()
+  const handleNextPage = () => {
+    router.push(`/test-companies?page=${page + 1}`)
+    setPage(page + 1)
+  }
+  const handlePrevPage = () => {
+    router.push(`/test-companies?page=${page - 1}`)
+    setPage(page - 1)
+  }
 
   useEffect(() => {
     (async () => {
       const params = new URLSearchParams()
       params.append("sort_type", "net_sales")
-      params.append("page", "1")
+      params.append("page", page.toString())
       params.append("q", "")
+
       const response = await fetch(`http://localhost:3003/api/v1/companies?${params.toString()}`)
       const res = await response.json()
-      const { companies, meta }: { companies: EachCompanyJSON[], meta: MetaJSON } = res
-      const { from } = meta
 
-      setFrom(from)
+      const { companies, meta }: { companies: EachCompanyJSON[], meta: MetaJSON } = res
+      const { page: pageData, from, prev, next } = meta
       setCompanies(companies)
+      setPage(pageData)
+      setFrom(from)
+      setPrev(prev)
+      setNext(next)
     })()
-  }, [])
+  }, [page])
 
   return (
     <>
-      <CompanyTable
-        companies={companies}
-        from={from}
+      {/* companies */}
+      <CompanyTable companies={companies} from={from} />
+      {/* companies */}
+
+      {/* pagination */}
+      <Pagination
+        prevRef={{ pathname: '/test-companies', query: { page: page - 1 }}}
+        nextRef={{ pathname: '/test-companies', query: { page: page + 1 }}}
+        page={page}
+        prev={prev}
+        next={next}
+        handlePrevPage={handlePrevPage}
+        handleNextPage={handleNextPage}
       />
+      {/* pagination */}
     </>
   )
 }
