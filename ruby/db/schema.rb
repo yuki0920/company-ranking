42fa037efc1a84f77f9a025ed2e30ca2f6eed8b3
yuki0920 <ywsep20@gmail.com>
2023-06-27 21:55:45 +0900

    Add capital columns to documents

--- ruby/db/schema.rb
+++ ruby/db/schema.rb
@@ -1,4 +1,18 @@
-ActiveRecord::Schema.define(version: 2023_06_02_000314) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_06_27_125325) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "documents", force: :cascade do |t|
@@ -38,6 +52,8 @@ ActiveRecord::Schema.define(version: 2023_06_02_000314) do
     t.bigint "net_cash_provided_by_used_in_financing_activities"
     t.bigint "cash_and_cash_equivalents"
     t.integer "consolidated_number_of_employees"
+    t.bigint "total_number_of_issued_shares"
+    t.float "payout_ratio"
     t.index ["document_id"], name: "index_documents_on_document_id", unique: true
     t.index ["security_code"], name: "index_documents_on_security_code"
   end
