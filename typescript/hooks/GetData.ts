eeda60dcdc218d4c90507f5f2866cc4121d87ab2
yuki0920 <ywsep20@gmail.com>
2023-07-04 21:28:58 +0900

    Add revalidate

--- typescript/hooks/GetData.ts
+++ typescript/hooks/GetData.ts
@@ -9,6 +9,10 @@ import { NEXT_PUBLIC_API_URL } from "@/constant"
 const config = new Configuration({ basePath: NEXT_PUBLIC_API_URL })
 const api = new DefaultApi(config)
 
+// NOTE: https://nextjs.org/docs/app/building-your-application/data-fetching/revalidating#background-revalidation
+// revalidate is the duration of the cache in seconds
+const defaultRevalidate = 60 * 60 // 1 hour
+
 export const listCompanies = async ({
   page,
   sortType,
@@ -26,31 +30,31 @@ export const listCompanies = async ({
   if (marketId !== null) params["marketId"] = marketId
   if (industryId !== null) params["industryId"] = industryId
 
-  const { companies, meta } = await api.listCompanies(params)
+  const { companies, meta } = await api.listCompanies(params, { next: { revalidate: defaultRevalidate } })
   return { companies, meta }
 }
 
 export const getCompany = async ({ code }: { code: number }) => {
-  const { company } = await api.getCompany({ code })
+  const { company } = await api.getCompany({ code }, { next: { revalidate: defaultRevalidate } })
   return company
 }
 
 export const getMarket = async ({ id }: { id: number }) => {
-  const { market } = await api.getMarket({ id })
+  const { market } = await api.getMarket({ id }, { next: { revalidate: defaultRevalidate } })
   return market
 }
 
 export const getMarkets = async () => {
-  const { markets } = await api.listMarkets()
+  const { markets } = await api.listMarkets({ next: { revalidate: defaultRevalidate } })
   return markets
 }
 
 export const getIndustry = async ({ id }: { id: number }) => {
-  const { industry } = await api.getIndustry({ id })
+  const { industry } = await api.getIndustry({ id }, { next: { revalidate: defaultRevalidate } })
   return industry
 }
 
 export const getIndustries = async () => {
-  const { industries } = await api.listIndustries()
+  const { industries } = await api.listIndustries({ next: { revalidate: defaultRevalidate } })
   return industries
 }
