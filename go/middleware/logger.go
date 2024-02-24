3356a918133af5cf701c7518bdfef622a32d126c
yuki0920 <ywsep20@gmail.com>
2024-02-24 15:18:51 +0900

    Remove `/api/v1/companies/:id` access from log

--- go/middleware/logger.go
+++ go/middleware/logger.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"log/slog"
 	"net/http"
+	"regexp"
 	"time"
 
 	"github.com/go-chi/chi/middleware"
@@ -14,14 +15,18 @@ func LoggingMiddleware() func(next http.Handler) http.Handler {
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 			t1 := time.Now()
 			defer func() {
-				slog.Info("Served",
-					slog.String("proto", r.Proto),
-					slog.String("method", r.Method),
-					slog.String("path", r.URL.Path),
-					slog.Duration("lat", time.Since(t1)),
-					slog.Int("status", ww.Status()),
-					slog.Int("size", ww.BytesWritten()),
-				)
+				re := regexp.MustCompile(`/api/v1/companies/\d+`)
+				path := r.URL.Path
+				if !re.MatchString(path) {
+					slog.Info("Served",
+						slog.String("proto", r.Proto),
+						slog.String("method", r.Method),
+						slog.String("path", r.URL.Path),
+						slog.Duration("lat", time.Since(t1)),
+						slog.Int("status", ww.Status()),
+						slog.Int("size", ww.BytesWritten()),
+					)
+				}
 			}()
 
 			next.ServeHTTP(ww, r)
