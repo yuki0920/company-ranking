dad48b2ec1b5fb18cc6a26cfaadeb2be8b6f21b9
yuki0920 <ywsep20@gmail.com>
2024-03-02 15:03:56 +0900

    Remove unnecessary fields

--- terraform/prod/google_cloud_run_v2_service.tf
+++ terraform/prod/google_cloud_run_v2_service.tf
@@ -1,44 +1,24 @@
-import {
-  id = "asia-northeast1/company-ranking-server"
-  to = google_cloud_run_v2_service.company_ranking_server
-}
-
 resource "google_cloud_run_v2_service" "company_ranking_server" {
-  annotations      = {}
-  client           = "gcloud"
-  client_version   = "463.0.0"
-  custom_audiences = []
-  description      = null
   ingress          = "INGRESS_TRAFFIC_ALL"
-  labels           = {}
-  launch_stage     = "GA"
   location         = "asia-northeast1"
   name             = "company-ranking-server"
   project          = "company-ranking-prod"
   template {
-    annotations                      = {}
-    encryption_key                   = null
-    execution_environment            = null
-    labels                           = {}
     max_instance_request_concurrency = 80
     revision                         = null
     service_account                  = "1026927710795-compute@developer.gserviceaccount.com"
-    session_affinity                 = false
     timeout                          = "300s"
     containers {
       args        = []
       command     = []
-      depends_on  = []
-      image       = "gcr.io/company-ranking-prod/company-ranking-server:51bf85a5d2d829bde391dc67198af7b5d4b529d6"
+      image       = ""
       name        = "company-ranking-server-1"
-      working_dir = null
       env {
         name  = "FRONT_URL"
         value = "company-ranking.net"
       }
       env {
         name  = "DATABASE_URL"
-        value = null
         value_source {
           secret_key_ref {
             secret  = "DATABASE_URL"
@@ -75,8 +55,14 @@ resource "google_cloud_run_v2_service" "company_ranking_server" {
   }
   traffic {
     percent  = 100
-    revision = null
-    tag      = null
     type     = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      client,
+      client_version,
+      template.0.containers.0.image
+    ]
   }
 }
