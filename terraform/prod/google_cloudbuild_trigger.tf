3cf271abca0d2d50ec8ec1082069cf0dc94f9327
yuki0920 <ywsep20@gmail.com>
2024-03-02 16:58:40 +0900

    Import google_cloudbuild_trigger

---
+++ terraform/prod/google_cloudbuild_trigger.tf
@@ -0,0 +1,43 @@
+import {
+  id = "company-ranking-job-deploy"
+  to = google_cloudbuild_trigger.company_ranking_job_deploy
+}
+
+import {
+  id = "company-ranking-server-deploy"
+  to = google_cloudbuild_trigger.company_ranking_server_deploy
+}
+
+resource "google_cloudbuild_trigger" "company_ranking_job_deploy" {
+  filename           = "ruby/cloudbuild.yaml"
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+  included_files     = ["ruby/**"]
+  location           = "global"
+  name               = "company-ranking-job-deploy"
+  project            = "company-ranking-prod"
+  github {
+    name  = "company-ranking"
+    owner = "yuki0920"
+    push {
+      branch       = "^main$"
+    }
+  }
+}
+
+
+resource "google_cloudbuild_trigger" "company_ranking_server_deploy" {
+  filename           = "go/cloudbuild.yaml"
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+  included_files     = ["go/**"]
+  location           = "global"
+  name               = "company-ranking-server-deploy"
+  project            = "company-ranking-prod"
+  github {
+    enterprise_config_resource_name = null
+    name                            = "company-ranking"
+    owner                           = "yuki0920"
+    push {
+      branch       = "^main$"
+    }
+  }
+}
