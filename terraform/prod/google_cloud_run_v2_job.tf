e99a8f44c04d69a8b246e0c25716c18b23f390ac
yuki0920 <ywsep20@gmail.com>
2024-03-02 15:44:04 +0900

    Refactor google_cloud_run_v2_job

--- terraform/prod/google_cloud_run_v2_job.tf
+++ terraform/prod/google_cloud_run_v2_job.tf
@@ -1,54 +1,40 @@
-import {
-  id = "asia-northeast1/company-ranking-job"
-  to = google_cloud_run_v2_job.company_ranking_job
+locals {
+  jobs = {
+    save-document-detail-job = {
+      args = ["bin/rake", "save_document_detail:batch"]
+    },
+    destroy-old-documents = {
+      args = ["bin/rake", "destroy_old_documents:execute"]
+    },
+    save-document-summary-daily-job = {
+      args = ["bin/rake", "save_document_summary:day"]
+    },
+    company-ranking-job = {
+      args = []
+    },
+    save-securities-job = {
+      args = ["bin/rake", "save_securities:every_2weeks"]
+    }
+  }
 }
 
-import {
-  id = "asia-northeast1/destroy-old-documents"
-  to = google_cloud_run_v2_job.destroy_old_documents
-}
+resource "google_cloud_run_v2_job" "job" {
+  for_each = local.jobs
 
-import {
-  id = "asia-northeast1/save-document-detail-job"
-  to = google_cloud_run_v2_job.save_document_detail_job
-}
-
-import {
-  id = "asia-northeast1/save-document-summary-daily-job"
-  to = google_cloud_run_v2_job.save_document_summary_daily_job
-}
-
-import {
-  id = "asia-northeast1/save-securities-job"
-  to = google_cloud_run_v2_job.save_securities_job
-}
-
-resource "google_cloud_run_v2_job" "save_document_detail_job" {
-  annotations    = {}
-  client         = "gcloud"
-  client_version = "463.0.0"
-  labels         = {}
-  launch_stage   = "GA"
-  location       = "asia-northeast1"
-  name           = "save-document-detail-job"
-  project        = "company-ranking-prod"
+  location = "asia-northeast1"
+  name     = each.key
+  project  = "company-ranking-prod"
   template {
-    annotations = {}
-    labels      = {}
-    parallelism = 0
     task_count  = 1
     template {
-      encryption_key        = null
       execution_environment = "EXECUTION_ENVIRONMENT_GEN2"
       max_retries           = 1
       service_account       = "1026927710795-compute@developer.gserviceaccount.com"
       timeout               = "600s"
       containers {
-        args        = ["bin/rake", "save_document_detail:batch"]
+        args        = each.value.args
         command     = []
-        image       = "gcr.io/company-ranking-prod/company-ranking-job:51bf85a5d2d829bde391dc67198af7b5d4b529d6"
-        name        = null
-        working_dir = null
+        image       = ""
         env {
           name  = "RAILS_ENV"
           value = "production"
@@ -59,7 +45,6 @@ resource "google_cloud_run_v2_job" "save_document_detail_job" {
         }
         env {
           name  = "DATABASE_URL"
-          value = null
           value_source {
             secret_key_ref {
               secret  = "DATABASE_URL"
@@ -69,7 +54,6 @@ resource "google_cloud_run_v2_job" "save_document_detail_job" {
         }
         env {
           name  = "SECRET_KEY_BASE"
-          value = null
           value_source {
             secret_key_ref {
               secret  = "SECRET_KEY_BASE"
@@ -86,264 +70,11 @@ resource "google_cloud_run_v2_job" "save_document_detail_job" {
       }
     }
   }
-}
-
-resource "google_cloud_run_v2_job" "destroy_old_documents" {
-  annotations    = {}
-  client         = "gcloud"
-  client_version = "463.0.0"
-  labels         = {}
-  launch_stage   = "GA"
-  location       = "asia-northeast1"
-  name           = "destroy-old-documents"
-  project        = "company-ranking-prod"
-  template {
-    annotations = {}
-    labels      = {}
-    parallelism = 0
-    task_count  = 1
-    template {
-      encryption_key        = null
-      execution_environment = "EXECUTION_ENVIRONMENT_GEN2"
-      max_retries           = 1
-      service_account       = "1026927710795-compute@developer.gserviceaccount.com"
-      timeout               = "600s"
-      containers {
-        args        = ["bin/rake", "destroy_old_documents:execute"]
-        command     = []
-        image       = "gcr.io/company-ranking-prod/company-ranking-job:51bf85a5d2d829bde391dc67198af7b5d4b529d6"
-        name        = null
-        working_dir = null
-        env {
-          name  = "RAILS_ENV"
-          value = "production"
-        }
-        env {
-          name  = "RAILS_LOG_TO_STDOUT"
-          value = "enabled"
-        }
-        env {
-          name  = "DATABASE_URL"
-          value = null
-          value_source {
-            secret_key_ref {
-              secret  = "DATABASE_URL"
-              version = "latest"
-            }
-          }
-        }
-        env {
-          name  = "SECRET_KEY_BASE"
-          value = null
-          value_source {
-            secret_key_ref {
-              secret  = "SECRET_KEY_BASE"
-              version = "latest"
-            }
-          }
-        }
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "512Mi"
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_v2_job" "save_document_summary_daily_job" {
-  annotations    = {}
-  client         = "gcloud"
-  client_version = "463.0.0"
-  labels         = {}
-  launch_stage   = "GA"
-  location       = "asia-northeast1"
-  name           = "save-document-summary-daily-job"
-  project        = "company-ranking-prod"
-  template {
-    annotations = {}
-    labels      = {}
-    parallelism = 0
-    task_count  = 1
-    template {
-      encryption_key        = null
-      execution_environment = "EXECUTION_ENVIRONMENT_GEN2"
-      max_retries           = 1
-      service_account       = "1026927710795-compute@developer.gserviceaccount.com"
-      timeout               = "600s"
-      containers {
-        args        = ["bin/rake", "save_document_summary:day"]
-        command     = []
-        image       = "gcr.io/company-ranking-prod/company-ranking-job:51bf85a5d2d829bde391dc67198af7b5d4b529d6"
-        name        = null
-        working_dir = null
-        env {
-          name  = "RAILS_ENV"
-          value = "production"
-        }
-        env {
-          name  = "RAILS_LOG_TO_STDOUT"
-          value = "enabled"
-        }
-        env {
-          name  = "DATABASE_URL"
-          value = null
-          value_source {
-            secret_key_ref {
-              secret  = "DATABASE_URL"
-              version = "latest"
-            }
-          }
-        }
-        env {
-          name  = "SECRET_KEY_BASE"
-          value = null
-          value_source {
-            secret_key_ref {
-              secret  = "SECRET_KEY_BASE"
-              version = "latest"
-            }
-          }
-        }
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "512Mi"
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_v2_job" "company_ranking_job" {
-  annotations    = {}
-  client         = "gcloud"
-  client_version = "463.0.0"
-  labels         = {}
-  launch_stage   = "GA"
-  location       = "asia-northeast1"
-  name           = "company-ranking-job"
-  project        = "company-ranking-prod"
-  template {
-    annotations = {}
-    labels      = {}
-    parallelism = 0
-    task_count  = 1
-    template {
-      encryption_key        = null
-      execution_environment = "EXECUTION_ENVIRONMENT_GEN2"
-      max_retries           = 1
-      service_account       = "1026927710795-compute@developer.gserviceaccount.com"
-      timeout               = "600s"
-      containers {
-        args        = []
-        command     = []
-        image       = "gcr.io/company-ranking-prod/company-ranking-job:51bf85a5d2d829bde391dc67198af7b5d4b529d6"
-        name        = null
-        working_dir = null
-        env {
-          name  = "RAILS_ENV"
-          value = "production"
-        }
-        env {
-          name  = "RAILS_LOG_TO_STDOUT"
-          value = "enabled"
-        }
-        env {
-          name  = "DATABASE_URL"
-          value = null
-          value_source {
-            secret_key_ref {
-              secret  = "DATABASE_URL"
-              version = "latest"
-            }
-          }
-        }
-        env {
-          name  = "SECRET_KEY_BASE"
-          value = null
-          value_source {
-            secret_key_ref {
-              secret  = "SECRET_KEY_BASE"
-              version = "latest"
-            }
-          }
-        }
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "512Mi"
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_v2_job" "save_securities_job" {
-  annotations    = {}
-  client         = "gcloud"
-  client_version = "463.0.0"
-  labels         = {}
-  launch_stage   = "GA"
-  location       = "asia-northeast1"
-  name           = "save-securities-job"
-  project        = "company-ranking-prod"
-  template {
-    annotations = {}
-    labels      = {}
-    parallelism = 0
-    task_count  = 1
-    template {
-      encryption_key        = null
-      execution_environment = "EXECUTION_ENVIRONMENT_GEN2"
-      max_retries           = 1
-      service_account       = "1026927710795-compute@developer.gserviceaccount.com"
-      timeout               = "600s"
-      containers {
-        args        = ["bin/rake", "save_securities:every_2weeks"]
-        command     = []
-        image       = "gcr.io/company-ranking-prod/company-ranking-job:51bf85a5d2d829bde391dc67198af7b5d4b529d6"
-        name        = null
-        working_dir = null
-        env {
-          name  = "RAILS_ENV"
-          value = "production"
-        }
-        env {
-          name  = "RAILS_LOG_TO_STDOUT"
-          value = "enabled"
-        }
-        env {
-          name  = "DATABASE_URL"
-          value = null
-          value_source {
-            secret_key_ref {
-              secret  = "DATABASE_URL"
-              version = "latest"
-            }
-          }
-        }
-        env {
-          name  = "SECRET_KEY_BASE"
-          value = null
-          value_source {
-            secret_key_ref {
-              secret  = "SECRET_KEY_BASE"
-              version = "latest"
-            }
-          }
-        }
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "512Mi"
-          }
-        }
-      }
-    }
+  lifecycle {
+    ignore_changes = [
+      client,
+      client_version,
+      template.0.template.0.containers.0.image
+    ]
   }
 }
