ebfcde2cc515a7ff549dad69ad011c8ce4472b82
yuki0920 <ywsep20@gmail.com>
2025-01-09 21:55:24 +0900

    Add test server

--- terraform/prod/google_cloud_run_v2_service.tf
+++ terraform/prod/google_cloud_run_v2_service.tf
@@ -57,3 +57,62 @@ resource "google_cloud_run_v2_service" "company_ranking_server" {
     ]
   }
 }
+
+resource "google_cloud_run_v2_service" "company_ranking_test_server" {
+  ingress  = "INGRESS_TRAFFIC_ALL"
+  location = "asia-northeast1"
+  name     = "company-ranking-test-server"
+  project  = "company-ranking-prod"
+  template {
+    max_instance_request_concurrency = 80
+    service_account                  = "1026927710795-compute@developer.gserviceaccount.com"
+    timeout                          = "300s"
+    containers {
+      image   = "gcr.io/company-ranking-prod/company-ranking-server:latest"
+      name    = "company-ranking-server-1"
+      env {
+        name  = "FRONT_URL"
+        value = "company-ranking.net"
+      }
+      env {
+        name = "DATABASE_URL"
+        value_source {
+          secret_key_ref {
+            secret  = "DATABASE_TEST_URL"
+            version = "latest"
+          }
+        }
+      }
+      ports {
+        container_port = 8080
+        name           = "http1"
+      }
+      resources {
+        cpu_idle = true
+        limits = {
+          cpu    = "1000m"
+          memory = "512Mi"
+        }
+      }
+      startup_probe {
+        failure_threshold     = 1
+        period_seconds        = 240
+        timeout_seconds       = 240
+        tcp_socket {
+          port = 8080
+        }
+      }
+    }
+    scaling {
+      max_instance_count = 1
+      min_instance_count = 0
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      client,
+      client_version,
+    ]
+  }
+}
--- terraform/prod/google_secret_manager_secret.tf
+++ terraform/prod/google_secret_manager_secret.tf
@@ -6,6 +6,14 @@ resource "google_secret_manager_secret" "database_url" {
   secret_id = "DATABASE_URL"
 }
 
+resource "google_secret_manager_secret" "database_test_url" {
+  project = "company-ranking-prod"
+  replication {
+    auto {}
+  }
+  secret_id = "DATABASE_TEST_URL"
+}
+
 resource "google_secret_manager_secret" "secret_key_base" {
   project = "company-ranking-prod"
   replication {
